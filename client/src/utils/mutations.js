7bd1f08508239eb07b5d63b610c5efdb1328ada6
Jakewilly14 <jakewilly14@gmail.com>
2024-03-05 18:50:28 -0600

    Client/Utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($username: String!, $_id: String!) {
+    addFriend(username: $username, _id: $_id) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
