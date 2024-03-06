84d5479eb10771f5bee4618a4d98dff0773e99ec
Jakewilly14 <jakewilly14@gmail.com>
2024-03-05 21:33:51 -0600

    Server-Routes

--- server/server.js
+++ server/server.js
@@ -12,13 +12,26 @@ const { PubSub } = require('graphql-subscriptions');
 const pubsub = new PubSub();
 // Socket.io imports
 const http = require('http')
+const cors = require('cors');
 
 const db = require('./config/connection');
 
+const userRoutes = require("./routes/userRoutes");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+// Setting up our user auth routes
+app.use('/users', userRoutes);
+
 const httpServer = http.createServer(app)
-const io = require("socket.io")(httpServer);
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000/messages',
+    methods: ['GET', 'POST'],
+    credentials: true  // Allow cookies, if applicable
+  }
+});
+
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 const apolloServer = new ApolloServer({
   schema,
