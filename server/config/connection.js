cdfbded6e6958bb49233b8319adfeb4d3127bd86
Timbir <tmiddlebrooks99@gmail.com>
2024-03-09 22:22:34 -0600

    deploying5

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ChitChatDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chitchat');
 
 module.exports = mongoose.connection;
