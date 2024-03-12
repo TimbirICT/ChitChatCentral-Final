2a62d6d1c92eacb49b6257a4ac8c6bbd057b4f6b
Timbir <tmiddlebrooks99@gmail.com>
2024-03-11 22:23:19 -0500

    fixed timbir avater

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -2,4 +2,5 @@ export {default as profile} from './profile.jpg'
 export {default as profile_Logo} from './profile_Logo.png'
 export {default as Jacob_Avatar} from './Jacob_Avatar.png'
 export {default as jon_profile} from './jon_profile.jpg'
+export {default as IMG_2894} from './IMG_2894.png'
 
